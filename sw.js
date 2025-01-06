6090a89ae6ddbab2a93c78c77d0d69983deddee2
BlackSourceLLC <blacksourcellc@bytarch.bz>
2025-01-05 23:58:58 -0600

    Initial commit of project files

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'genie-cache-v1';
+const urlsToCache = [
+  '/',
+  '/offline.html',
+  '/logo.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          
+          return fetch(event.request).catch(() => {
+            // Serve custom offline page
+            return caches.match('/offline.html') || 
+                   new Response('Offline', { 
+                     headers: { 'Content-Type': 'text/html' } 
+                   });
+          });
+        })
+    );
+  });
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
